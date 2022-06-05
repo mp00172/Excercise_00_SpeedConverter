f6332ad9e290220e49e60aada33ab01f5043db5c
mp00172 <61931347+mp00172@users.noreply.github.com>
2022-06-05 18:32:04 +0200

    Add files via upload

---
+++ Exercise_00_SpeedConverter/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println(SpeedConverter.toMilesPerHour(10.5));
+        SpeedConverter.printConversion(10.5);
+    }
+}
